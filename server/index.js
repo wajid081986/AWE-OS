d40bd41b4b6e8493ace02a6282b4155ca939d192
wajid081986 <wajid081986@gmail.com>
2026-04-25 21:45:00 +0530

    fix: trust proxy for Render deployment

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ require('./jobs/revenue.cron');                                                 
 require('./jobs/support.cron');
 
 const app  = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // ===== ENV CHECK =====
